edca00467625d585663781ab6dba98a3b675b8d9
AnotherPillow <Thepilous_@outlook.com>
2023-02-12 12:33:39 +0000

    Make installer, add phantom cape, add README

---
+++ README.md
@@ -0,0 +1,28 @@
+# Phantom Capes
+
+A custom Optifine cape server.
+
+## Setup
+
+- Install [Python 3.8 or higher](https://www.python.org/downloads/)
+- Download the latest release from the [releases page](https://github.com/anotherpillow/phantom-capes/releases)
+- Run the installer as an administrator.
+- Restart your computer or run `ipconfig /flushdns` in an administrator command prompt.
+- Run app.py in the `server` folder.
+- Navigate to `s.optifine.net` in your browser.
+- Enter your IGN and choose a cape, then press `Submit`.
+- Start Minecraft, if it was already open, refresh your Optifine cape via `Game Menu > Options > Skin Customisation > Optifine Cape > Reload Cape`
+
+## FAQ
+
+### Can other people see my cape?
+
+No, only you can see your cape.
+
+### Does this work with Lunar Client?
+
+Yes, it does.
+
+### Does this work on vanilla Minecraft or without Optifine?
+
+No, it does not.
---
+++ phantom-installer/src/main.rs
@@ -0,0 +1,52 @@
+use is_superuser::is_superuser;
+use std::fs;
+use regex::Regex;
+
+fn main() {
+    let optifine_regex = Regex::new(r"^(\d+\.)+(\d+) (ph|s).(optifine|lunarclientcdn)\.(.+)$").unwrap();
+    if !is_superuser() {
+        println!("Please run as admin");
+        //wait for input before closing 
+        get_input();
+        return;
+    }
+    println!("Running as admin");
+    let hosts_path = "C:\\Windows\\System32\\drivers\\etc\\hosts";
+    let hosts = fs::read_to_string(hosts_path).unwrap();
+    let lines_lines = hosts.lines();
+    //turn lines into an array of lines
+    let mut lines: Vec<&str> = lines_lines.clone().collect();
+
+    for line in lines_lines.into_iter() {
+        if optifine_regex.is_match(line) {
+            //remove it from lines
+            println!("Removing line: {}", line);
+            lines.retain(|&x| x != line);
+        }
+    }
+    let lines_to_add = vec![
+        "",
+        "# Phantom Capes",
+        "127.0.0.1 s.optifine.net",
+        "127.0.0.1 s-optifine.lunarclientcdn.com",
+        "107.182.233.85 ph.optifine.net",
+        "107.182.233.85 ph.lunarclientcdn.com",
+    ];
+    for line in lines_to_add.into_iter() {
+        lines.push(line);
+    }
+    let new_hosts = lines.join("\n");
+    println!("Writing new hosts file");
+    fs::write(hosts_path, new_hosts).unwrap();
+    println!("Done");
+
+    get_input()
+
+
+}
+
+fn get_input() {
+    let mut input = String::new();
+    println!("Press any key to continue...");
+    std::io::stdin().read_line(&mut input).unwrap();
+}
